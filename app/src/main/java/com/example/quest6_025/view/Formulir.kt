fcae6ef95f5c28f4cd26f440ca0364cfc4e0cc97
humannocturnal <syaputrar374@gmail.com>
2025-11-21 16:49:44 +0700

    menambahkan divider, input alamat, dan tombol submit pada FormIsian

--- app/src/main/java/com/example/quest6_025/view/Formulir.kt
+++ app/src/main/java/com/example/quest6_025/view/Formulir.kt
@@ -3,8 +3,12 @@ package com.example.quest6_025.view
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.DividerDefaults.Thickness
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
@@ -63,6 +67,28 @@ fun FormIsian(
                         Text(text = item)
                     }
                 }
+            }
+            HorizontalDivider(modifier = Modifier
+                .padding(all = 20.dp)
+                .width(width = 250.dp),
+                thickness = 1.dp,
+                color = Color.Red
+            )
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .width(width = 250.dp),
+                label = {Text(text="Alamat")},
+                onValueChange = {},
+            )
+            Spacer(modifier = Modifier.height(height = 30.dp))
+            Button(
+                modifier = Modifier.fillMaxWidth(fraction = 1f)
+                    .padding(all= 25.dp),
+                onClick = OnSubmitBtnClick
+            ) {
+                Text (stringResource(id = R.string.submit))
             }
         }
     }
