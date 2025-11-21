d277a17d3f0f5314dec29e9fbe558c439ca1e56d
humannocturnal <syaputrar374@gmail.com>
2025-11-21 17:04:38 +0700

    menambahkan navigasi FormIsian ke TampilData beserta fungsi kembali

--- app/src/main/java/com/example/quest6_025/Navigasi.kt
+++ app/src/main/java/com/example/quest6_025/Navigasi.kt
@@ -1,8 +1,15 @@
 package com.example.quest6_025
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.quest6_025.view.FormIsian
+import com.example.quest6_025.view.TampilData
 
 enum class Navigasi {
     Formulirku,
@@ -12,5 +19,36 @@ enum class Navigasi {
 fun DataApp(
     navController: NavHostController = rememberNavController()
 ) {
+    Scaffold { isiRuang ->
 
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulirku.name,
+            modifier = Modifier.padding(paddingValues = isiRuang)
+        ) {
+            composable(route = Navigasi.Formulirku.name) {
+                FormIsian (
+                    OnSubmitBtnClick = {
+                        navController.navigate(route = Navigasi.Detail.name)
+                    }
+                )
+            }
+            composable(route = Navigasi.Detail.name) {
+                TampilData (
+                    onBackBtnClick = {
+                        cancelAndBackToFormulir(navController)
+                    }
+                )
+            }
+        }
+    }
+}
+
+private fun cancelAndBackToFormulir(
+    navController: NavHostController
+){
+    navController.popBackStack(
+        route = Navigasi.Formulirku.name,
+        inclusive = false
+    )
 }
