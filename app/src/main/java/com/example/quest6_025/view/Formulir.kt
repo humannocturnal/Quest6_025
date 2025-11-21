dfe064f2442e7c4c4f3ec6c2d0778f79f9c5b129
humannocturnal <syaputrar374@gmail.com>
2025-11-21 16:48:04 +0700

    menambahkan form input nama, divider, dan pilihan jenis kelamin pada FormIsian

--- app/src/main/java/com/example/quest6_025/view/Formulir.kt
+++ app/src/main/java/com/example/quest6_025/view/Formulir.kt
@@ -1,15 +1,27 @@
 package com.example.quest6_025.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.DividerDefaults.Thickness
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.quest6_025.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -25,6 +37,33 @@ fun FormIsian(
                     containerColor = colorResource(id = R.color.teal_700))
             )}
     ){isiRuang ->
-
+        Column (modifier = Modifier.padding(paddingValues = isiRuang),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally){
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .width(width = 250.dp),
+                label = {Text(text="nama Lengkap")},
+                onValueChange = {},
+            )
+            HorizontalDivider(modifier = Modifier
+                .padding(all = 20.dp)
+                .width(width = 250.dp), thickness = Thickness, color = Color.Red)
+            Row{
+                jenisK.forEach {
+                        item->
+                    Row ( verticalAlignment = Alignment.CenterVertically){
+                        RadioButton(
+                            selected = false,
+                            onClick = {item}
+                        )
+                        Text(text = item)
+                    }
+                }
+            }
+        }
     }
 }
